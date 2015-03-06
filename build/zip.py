cfccce270a6d7125d4c230a832dbfdfec4f1f5a6
crazyt <tobias15556@gmx.de>
2015-03-06 18:26:06 +0100

    Build normal zip for windows or other systems.

---
+++ build/zip.py
@@ -0,0 +1,13 @@
+import zipfile
+from os import walk
+
+f = []
+for (dirpath, dirnames, filenames) in walk("."):
+    f.extend(filenames)
+    break
+
+myZipFile = zipfile.ZipFile("dumpAnalyser_any.zip", "w" )
+for fn in f:
+    if fn.endswith(".py") or fn.endswith(".bat") or fn.endswith(".ui"):
+        myZipFile.write(fn, fn, zipfile.ZIP_DEFLATED )
+
